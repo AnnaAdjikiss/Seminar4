49eb585f95fd2fdd9cc104677eff0ed8f65ba113
anna.adjikiss <adjikiss@mail.ru>
2022-03-23 22:37:06 +0300

    task2 done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 1
 // Написать программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+/*
 int ArythmSum(int A)
 {
     int result = 0;
@@ -18,3 +19,23 @@ int ArythmSum(int A)
 Console.WriteLine("Введите целое положительное число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Сумма чисел от 1 до {numA} равна {ArythmSum(numA)}");
+*/
+
+
+//Задача 2
+// Программа принимает на вход число и выдает количество цифр в числе
+/*
+void Digits(int value)
+{
+    int lenght = 1;
+    while (Math.Abs(value / 10) > 1)
+    {
+        value = value / 10;
+        lenght++;
+    }
+    Console.WriteLine("количество цифр: " + lenght);
+}
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Digits(number);
+*/
